fbf056e40e7c46ccadbcb8067f846a74fce05ac1
shiroshiro-taka <potetikuitai39@gmail.com>
2026-04-11 23:47:40 +0900

    カレンダー不具合修正

--- src/main/java/com/example/learning_app/LearningAppApplication.java
+++ src/main/java/com/example/learning_app/LearningAppApplication.java
@@ -1,13 +1,22 @@
 package com.example.learning_app;
 
+import java.util.TimeZone;
+
+import jakarta.annotation.PostConstruct; // 追加
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearningAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LearningAppApplication.class, args);
-	}
+    // アプリケーション全体のデフォルトタイムゾーンをJSTに設定
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tokyo"));
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(LearningAppApplication.class, args);
+    }
 }
